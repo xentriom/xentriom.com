67a0858a61cf7d337db272a8ac37ddd3e9e345db
xentriom <c04jason@gmail.com>
2025-04-23 17:23:08 -0400

    metadata

---
+++ app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Xentriom',
+    short_name: 'Xentriom',
+    description: 'Next.js App',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#6366F1',
+    icons: [
+    {
+      src: "/web-app-manifest-192x192.png",
+      sizes: "192x192",
+      type: "image/png",
+      purpose: "maskable"
+    },
+    {
+      src: "/web-app-manifest-512x512.png",
+      sizes: "512x512",
+      type: "image/png",
+      purpose: "maskable"
+    }
+  ],
+  }
+}
