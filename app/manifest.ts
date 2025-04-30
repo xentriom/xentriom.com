83dd3bdeba1f371770cac8b1931456fb43be3b2f
xentriom <c04jason@gmail.com>
2025-04-30 18:51:16 -0400

    updated images name and pnpm

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,13 +11,13 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#6366F1',
     icons: [
       {
-        src: '/web-app-manifest-192x192.png',
+        src: '/manifest-192x192.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/web-app-manifest-512x512.png',
+        src: '/manifest-512x512.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
